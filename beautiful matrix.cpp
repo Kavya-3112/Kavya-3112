0447e2a72c24487c6a4b68c62bee2fa89ec0f66e
kavya K <125470243+Kavya-3112@users.noreply.github.com>
2023-08-31 22:40:17 +0530

    Create beautiful matrix.cpp

---
+++ beautiful matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int r,c;
+    for(int i=1;i<=5;i=i+1)
+    {
+        for(int j=1;j<=5;j=j+1)
+        {
+            int k;
+            cin>>k;
+            if(k==1)
+            {
+                cout<<abs(i-3)+abs(j-3)<<endl;
+                
+            }
+        }
+    }
+    
+    
+}
